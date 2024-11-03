9b2504d2b0534690b5ba3e921e7ce05eef1dff40
msllagas <mndyllagas@gmail.com>
2024-11-03 19:37:59 +0800

    chore: install and configure PrimeIcons

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import PrimeVue from 'primevue/config'
-import './style.css';
-import {definePreset} from "@primevue/themes";
+import { createApp } from "vue";
+import App from "./App.vue";
+import PrimeVue from "primevue/config";
+import "./style.css";
+import "primeicons/primeicons.css";
+import { definePreset } from "@primevue/themes";
 import Aura from "@primevue/themes/aura";
-import {createPinia} from "pinia";
+import { createPinia } from "pinia";
 import router from "./router";
 
 const app = createApp(App);
@@ -16,30 +17,30 @@ app.use(router);
 const auraPreset = definePreset(Aura, {
     semantic: {
         primary: {
-            50: '{blue.50}',
-            100: '{blue.100}',
-            200: '{blue.200}',
-            300: '{blue.300}',
-            400: '{blue.400}',
-            500: '{blue.500}',
-            600: '{blue.600}',
-            700: '{blue.700}',
-            800: '{blue.800}',
-            900: '{blue.900}',
-            950: '{blue.950}'
-        }
-    }
+            50: "{blue.50}",
+            100: "{blue.100}",
+            200: "{blue.200}",
+            300: "{blue.300}",
+            400: "{blue.400}",
+            500: "{blue.500}",
+            600: "{blue.600}",
+            700: "{blue.700}",
+            800: "{blue.800}",
+            900: "{blue.900}",
+            950: "{blue.950}",
+        },
+    },
 });
 
 app.use(PrimeVue, {
     theme: {
         preset: auraPreset,
         options: {
-            prefix: 'n',
-            darkModeSelector: false || 'none',
-            cssLayer: false
-        }
-    }
+            prefix: "n",
+            darkModeSelector: false || "none",
+            cssLayer: false,
+        },
+    },
 });
 
 app.mount("#app");
