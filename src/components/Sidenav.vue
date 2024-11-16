50d2fac10f8dc35dd53aa76416885011a8f078d9
msllagas <mndyllagas@gmail.com>
2024-11-16 18:39:54 +0800

    feat: update styling

--- src/components/Sidenav.vue
+++ src/components/Sidenav.vue
@@ -10,11 +10,65 @@
     function toggleDarkMode() {
         document.documentElement.classList.toggle("dark");
     }
+
+    const buttonDT = ref({
+        colorScheme: {
+            light: {
+                root: {
+                    secondary: {
+                        color: "#ffffff",
+                        background: "{primary.500}",
+                        borderColor: "transparent",
+                        hoverBackground: "{primary.600}",
+                        hoverColor: "#ffffff",
+                    },
+                },
+            },
+            dark: {
+                root: {
+                    secondary: {
+                        color: "#ffffff",
+                        background: "{primary.500}",
+                        hoverBackground: "{primary.400}",
+                        hoverColor: "#ffffff",
+                        borderColor: "transparent",
+                        hoverBorderColor: "{primary.500}",
+                        activeBackground: "{primary.600}",
+                        activeColor: "#ffffff",
+                        activeBorderColor: "transparent",
+                    },
+                },
+            },
+        },
+    });
+
+    const navLinks = ref({
+        colorScheme: {
+            light: {
+                root: {
+                    secondary: {
+                        color: "{gray.800}",
+                        background: "{gray.200}",
+                        hoverBackground: "{gray.300}",
+                    },
+                },
+            },
+            dark: {
+                root: {
+                    secondary: {
+                        color: "{gray.200}",
+                        background: "{gray.800}",
+                        hoverBackground: "{gray.700}",
+                    },
+                },
+            },
+        },
+    });
 </script>
 
 <template>
     <div
-        class="fixed left-0 top-0 z-50 flex h-screen flex-col bg-slate-50 dark:bg-zinc-900"
+        class="fixed left-0 top-0 z-50 flex h-screen flex-col bg-gray-100 dark:bg-gray-900"
         v-bind="$attrs">
         <div class="flex-none">
             <div class="px-2 pt-4 text-center">
@@ -33,12 +87,26 @@
                     <li>
                         <Button
                             v-tooltip.right="{
+                                value: 'New Note',
+                                pt: tooltipPassThrough,
+                            }"
+                            as="router-link"
+                            icon="pi pi-pen-to-square"
+                            rounded
+                            severity="secondary"
+                            :dt="buttonDT"
+                            :to="{ name: 'DashboardCompose' }" />
+                    </li>
+                    <li>
+                        <Button
+                            v-tooltip.right="{
                                 value: 'Home',
                                 pt: tooltipPassThrough,
                             }"
                             as="router-link"
                             icon="pi pi-home"
                             rounded
+                            :dt="navLinks"
                             severity="secondary"
                             :to="{ name: 'DashboardHome' }" />
                     </li>
@@ -52,6 +120,7 @@
                             icon="pi pi-bookmark"
                             severity="secondary"
                             rounded
+                            :dt="navLinks"
                             :to="{ name: 'DashboardBookmark' }" />
                     </li>
                     <li>
@@ -64,6 +133,7 @@
                             icon="pi pi-folder"
                             severity="secondary"
                             rounded
+                            :dt="navLinks"
                             :to="{ name: 'DashboardArchive' }" />
                     </li>
                     <li>
@@ -76,6 +146,7 @@
                             icon="pi pi-trash"
                             severity="secondary"
                             rounded
+                            :dt="navLinks"
                             :to="{ name: 'DashboardTrash' }" />
                     </li>
                 </ul>
@@ -89,18 +160,28 @@
                     class="m-0 flex list-none flex-col items-center gap-5 overflow-hidden p-2 pb-4">
                     <li>
                         <Button
+                            v-tooltip.right="{
+                                value: 'Toggle Theme',
+                                pt: tooltipPassThrough,
+                            }"
                             icon="pi pi-moon"
                             severity="secondary"
                             rounded
                             aria-label="Moon"
+                            :dt="navLinks"
                             @click="toggleDarkMode" />
                     </li>
                     <li>
                         <Button
+                            v-tooltip.right="{
+                                value: 'Profile',
+                                pt: tooltipPassThrough,
+                            }"
                             as="router-link"
                             icon="pi pi-user"
                             severity="secondary"
                             rounded
+                            :dt="navLinks"
                             :to="{ name: 'DashboardUserProfile' }" />
                     </li>
                 </ul>
@@ -115,7 +196,7 @@
         color: var(--n-surface-0);
     }
     li a.router-link-active:hover {
-        background-color: var(--n-primary-600);
+        background-color: var(--n-primary-400);
         color: var(--n-surface-0);
     }
 </style>
